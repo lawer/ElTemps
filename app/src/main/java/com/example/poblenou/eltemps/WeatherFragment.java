ede342da62e5fd04473d855f496f22f30af0b47f
carlesgm <carlesgm@gmail.com>
2015-10-18 23:48:39 +0200

    Add SettingsActivity

--- app/src/main/java/com/example/poblenou/eltemps/WeatherFragment.java
+++ app/src/main/java/com/example/poblenou/eltemps/WeatherFragment.java
@@ -84,11 +84,6 @@ public class WeatherFragment extends Fragment {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         if (id == R.id.action_refresh) {
             refresh();
             return true;
