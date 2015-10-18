8112b13d1530eebafd41e401f68024e901254d5c
carlesgm <carlesgm@gmail.com>
2015-10-19 00:19:11 +0200

    onResume

--- app/src/main/java/com/example/poblenou/eltemps/WeatherFragment.java
+++ app/src/main/java/com/example/poblenou/eltemps/WeatherFragment.java
@@ -37,6 +37,12 @@ public class WeatherFragment extends Fragment {
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+        refresh();
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
